99dd231f31bbcaa2482ecbaebb0b2bc0d88aa801
Ritik Mirase <ritikmirase16@gmail.com>
2025-09-11 23:32:44 +0530

    Create Assign4_Complex_Calci.cpp

---
+++ OOPS/Assign4_Complex_Calci.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+using namespace std;
+
+class complex {
+	public:
+	float real,img;
+
+	complex() {
+
+	real=0;
+	img=0;
+	}
+	void get(){
+		cout<<"Enter real no:";
+		cin>>real;
+		cout<<"Enter imaginary no:";
+		cin>>img;
+		}
+		void display() {
+			cout<<real<<"+"<<img<<"i"<<endl;
+			}
+		complex operator+(complex);
+		complex operator-(complex);
+		complex operator*(complex);
+};
+
+complex complex::operator+(complex obj) {
+	complex temp;
+	temp.real=this->real+obj.real;
+	temp.img=this->img+obj.img;
+	return temp;
+}
+
+complex complex::operator-(complex obj) {
+	complex temp;
+	temp.real=this->real-obj.real;
+	temp.img=this->img-obj.img;
+	return temp;
+}
+
+complex complex::operator*(complex obj){
+	complex temp;
+	temp.real=( this->real*obj.real)-(this->img*obj.real);
+	temp.img=( this->real*obj.img)+(this->img*obj.img);
+	return temp;
+}
+
+int main () {
+	complex C1,C2,C3,C4,C5;
+	C1.get();
+	C2.get();
+	C3=C1+C2;
+	C4=C1-C2;
+	C5=C1*C2;
+	
+	
+//Result
+	cout<<"\nC1 = ";
+	C1.display();
+	cout<<"C2 = ";
+	C2.display();
+	cout<<"C3 = ";
+	C3.display();
+	cout<<"C4 = ";
+	C4.display();
+	cout<<"C5 = ";
+	C5.display();
+	return 0;	
+	
+}
