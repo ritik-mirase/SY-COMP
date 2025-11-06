a0afed96fbdbd625cefcfc88c70180d3b0e6d58c
Ritik Mirase <ritikmirase16@gmail.com>
2025-11-06 22:14:45 +0530

    Refactor bubble sort and enhance output
    Refactored bubble sort implementation to improve readability and structure. Added detailed output after each sorting pass.

--- DS/Assign2A_BubbleSort.cpp
+++ DS/Assign2A_BubbleSort.cpp
@@ -1,32 +1,42 @@
 #include <iostream>
 using namespace std;
-int main(){
-	int n;
+int main()
+{
+    int n, i;
+    int temp, j;
+    int player[12];
+    cout << "Enter the no of players:";
+    cin >> n;
+    for (i = 0; i < n; i++)
+    {
+        cout << "Enter score of player:" << i + 1 << endl;
+        cin >> player[i];
+    }
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (player[j] > player[j + 1])
+            {
+                temp = player[j];
+                player[j] = player[j + 1];
+                player[j + 1] = temp;
+            }
+        }
 
-cout<<"How Many Players"<<endl;
-cin>>n;
-cout<<"Scores of Players: "<<n<<endl;
-int player[n];
-for(int i=0;i<n;i++){
-	cin>>player[i];
-}
-cout<<"Entered Scores"<<endl;
-for(int i=0;i<n;i++){
+        cout << "After pass " << i + 1 << ": ";
+        for (int k = 0; k < n; k++)
+        {
+            cout << player[k] << " ";
+        }
+        cout << endl;
+    }
 
-	cout<<player[i]<<endl;
-}
-for(int i=0;i<n-1;i++){
-	for(int j=0;j<n-1;j++){
-		if(player[j]>player[j+1]){
-			int temp;
-			temp=player[j];
-			player[j]=player[j+1];
-			player[j+1] = temp;}}}
-			
-			
-cout<<"After Sorting"<<endl;
-for(int i=0;i<n;i++){
-	cout<<player[i]<<endl;
-	}
-	return 0;
+    cout << "\nFinal sorted list is:" << endl;
+
+    for (i = 0; i < n; i++)
+    {
+        cout << "Player " << i + 1 << " sorted is " << player[i] << endl;
+    }
+    return 0;
 }
